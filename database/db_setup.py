51ead87c0bf80fec51a423b01ee340262e4ddc2f
johncarl2234 <acostajohncarl33@gmail.com>
2026-08-11 12:53:15 +0800

    Refactor database table definitions for clients, technicians, and tickets for consistency and clarity

--- database/db_setup.py
+++ database/db_setup.py
@@ -20,47 +20,50 @@ def cursor(connection):
 
 # Setup database interfaces for client, technicians, administrator
 
-# Parent table
-clients = """ CREATE TABLE IF NOT EXISTS clients (
+# Client and Technician Table
+clients = """ CREATE TABLE IF NOT EXISTS Clients (
         cli_id INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT,
         client TEXT NOT NULL,
-        contact TEXT NOT NULL
+        contact_number TEXT NOT NULL,
+        email TEXT,
+        address TEXT NOT NULL
+)
+"""
+technicians = f""" CREATE TABLE IF NOT EXISTS Agents (
+         agent_id INTEGER PRIMARY KEY AUTOINCREMENT,
+         agent TEXT,
+         assignment TEXT NOT NULL
 )
 """
 
-# Child table
-tickets = f""" CREATE TABLE IF NOT EXISTS tickets (
+# Ticket table (Parents: clients, technicians)
+tickets = f""" CREATE TABLE IF NOT EXISTS Tickets (
         order_id INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT,
-        ticket_number TEXT GENERATED ALWAYS AS ('SVC' || printf('%03d{date.today().year}', order_id)) STORED UNIQUE,
+        tkt_ref TEXT GENERATED ALWAYS AS ('SVC' || printf('%03d{date.today().year}', order_id)) STORED UNIQUE,
         order_date DATE,
-        ticket_info TEXT NOT NULL,
-        ticket_id INTEGER, 
-        FOREIGN KEY (ticket_id) REFERENCES clients(cli_id) ON DELETE CASCADE,
-        agent_id INTEGER DEFAULT 1,
-        FOREIGN KEY (agent_id) REFERENCES agents(tech_id) ON DELETE SET DEFAULT
-)
-"""
-
-technicians = f""" CREATE TABLE IF NOT EXISTS agents (
-        tech_id INTEGER UNIQUE PRIMARY KEY,
-        technician TEXT,
-        assignment VARCHAR(3)
+        tkt_inf TEXT NOT NULL,
+        resolved_at DATETIME DEFAULT NULL, 
+        status TEXT GENERATED ALWAYS AS (CASE WHEN resolved_at IS NULL THEN 'Open' ELSE 'Resolved' END) STORED,
+        tkt_id INTEGER,     
+        agent_id INT DEFAULT 1,
+        FOREIGN KEY (tkt_id) REFERENCES Clients(cli_id) ON DELETE CASCADE,
+        FOREIGN KEY (agent_id) REFERENCES Agents(agent_id) ON DELETE SET DEFAULT
 )
 """
 
 def run_setup():
-    try:
+    try: 
         cursor(setup_conn()).execute(clients)
         cursor(setup_conn()).execute(tickets)
         cursor(setup_conn()).execute(technicians)
         setup_conn().commit()
     except sqlite3.OperationalError as e:
-        print(f"Error: {e}")
-    finally:
+        print(f"Operational Error (Database): {e}")
+    finally: # runs regardless of the state
         setup_conn().close()
 
 if not file.is_file():
     if __name__ == "__main__":
         run_setup()
 else:
-    print(f"Setup will not execute since the file is found at {file}")
+    print(f"Database: Setup will not execute since the file is found at {file}")
